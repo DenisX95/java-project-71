07e7d1d07c9315507aa2e5a8a36e472294f09233
Klopov <klopov.1995@list.ru>
2024-08-27 20:27:10 +0300

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    application
+    java
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    // Входная точка
+    mainClass.set("hexlet.code.App")
+}
