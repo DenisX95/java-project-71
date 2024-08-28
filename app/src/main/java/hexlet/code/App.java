02d3354349c9c9b2c9cd0d35f43c470f47934798
Klopov <klopov.1995@list.ru>
2024-08-28 20:45:36 +0300

    update App.java
    add standatr options -h & -V

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+//import picocli.CommandLine.Option;
+
+@Command(name = "app", mixinStandardHelpOptions = true, version = "app 1.0",
+        description = "Print standart greeting")
+public class App implements Runnable {
+
+    //@Option(names = {"-h", "--help"}, description = "Show help info")
+    //private boolean help;
+
+    @Override
+    public void run() {
         System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        CommandLine.run(new App(), args);
     }
 }
