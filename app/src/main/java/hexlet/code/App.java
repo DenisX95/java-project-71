636ef25d0d80f5f42031bd6759642a89e6385f5b
Klopov <klopov.1995@list.ru>
2024-08-28 21:26:28 +0300

    update App.java
    add parameters: filepath1, filepath2.
    add option: -f.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,14 +2,21 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-//import picocli.CommandLine.Option;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "app", mixinStandardHelpOptions = true, version = "app 1.0",
-        description = "Print standart greeting")
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "app 1.0",
+        description = "Compares two configuration files and shows a difference")
 public class App implements Runnable {
 
-    //@Option(names = {"-h", "--help"}, description = "Show help info")
-    //private boolean help;
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format;
 
     @Override
     public void run() {
